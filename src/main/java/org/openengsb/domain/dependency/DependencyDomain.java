0078833c957307a7bff89584dc9381cd55bb707b
Stefan Dösinger <stefandoesinger@gmail.com>
2012-04-24 22:49:50 +0200

    The merge method needs version information

--- src/main/java/org/openengsb/domain/dependency/DependencyDomain.java
+++ src/main/java/org/openengsb/domain/dependency/DependencyDomain.java
@@ -33,6 +33,6 @@ public interface DependencyDomain extends Domain {
      * asynchronously. The result can be retrieved using the events raised by this domain, which also contain the id.
      */
     @Raises({ MergeSuccessEvent.class, MergeFailEvent.class })
-    void merge(OpenEngSBFileModel path);
+    void merge(OpenEngSBFileModel path, String dependencyLocation);
 }
 // @extract-end
